004f9fbe7a8a6e9e9c4f7f1f7e368c6b2b35eed0
ATCBS <atcbs003@outlook.com>
2016-05-19 16:38:05 +0900

    第9章まで

--- lesson/src/servlet/FruitdoGet.java
+++ lesson/src/servlet/FruitdoGet.java
@@ -3,12 +3,12 @@ package servlet;
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import bean.Fruit;
 
@@ -18,10 +18,12 @@ public class FruitdoGet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Fruit f = new Fruit("いちご", 700);
-		HttpSession session = request.getSession();
-		session.setAttribute("fruit", f);
+		ServletContext application = this.getServletContext();
+		application.setAttribute("fruit", f);
+//		HttpSession session = request.getSession();
+//		session.setAttribute("fruit", f);
 //		request.setAttribute("fruit", f);
-		RequestDispatcher d = request.getRequestDispatcher("/show2.jsp");
+		RequestDispatcher d = request.getRequestDispatcher("/show3.jsp");
 		d.forward(request, response);
 	}
 }
